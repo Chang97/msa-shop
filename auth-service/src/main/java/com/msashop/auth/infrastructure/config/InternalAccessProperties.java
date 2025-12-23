0ccacf9ab4b7c8eb2a22db0c2a584d909ef12af7
Chang97 <ckdrbsaos@naver.com>
2025-12-23 21:11:41 +0900

    빈 생성 오류로 인해 @Component 제거

--- auth-service/src/main/java/com/msashop/auth/infrastructure/config/InternalAccessProperties.java
+++ auth-service/src/main/java/com/msashop/auth/infrastructure/config/InternalAccessProperties.java
@@ -1,14 +1,12 @@
 package com.msashop.auth.infrastructure.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Component
 @ConfigurationProperties(prefix = "security.internal")
 public class InternalAccessProperties {
 
